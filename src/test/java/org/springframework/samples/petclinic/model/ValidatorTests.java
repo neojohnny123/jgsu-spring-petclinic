054bc5ebee50ca7bb21dcea683c172744a08c575
ameen.sher@gmail.com <97098913+AmeenSher@users.noreply.github.com>
2022-06-19 00:21:26 +0100

    push

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("mad");
+		person.setFirstName("mad2");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
